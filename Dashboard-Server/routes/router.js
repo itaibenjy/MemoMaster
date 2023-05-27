5b75bb34eaa2b750b72e6a16a62411ae0a6b765d
Itai Benjamin <itaibenjy@gmail.com>
2023-05-27 19:47:30 +0300

    update

--- Dashboard-Server/routes/router.js
+++ Dashboard-Server/routes/router.js
@@ -1,8 +1,15 @@
 const express = require('express');
+
+// import routes
 const userRouter = require('./userRouter');
+const toDoRouter = require('./toDoRouter');
+
 const router = express.Router();
 
 // user routes
 router.use('/api/user', userRouter);
+
+// To Do routes
+router.use('/api/todo', toDoRouter);
 
 module.exports = router;
