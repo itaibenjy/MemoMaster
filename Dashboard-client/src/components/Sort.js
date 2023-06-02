b297cf47a437814c4bbed985942f51135b07d8d5
itaibenjy <itaibenjy@gmail.com>
2023-06-02 15:15:30 +0300

    removing console.logs

--- Dashboard-client/src/components/Sort.js
+++ Dashboard-client/src/components/Sort.js
@@ -7,8 +7,8 @@ export default function Sort({reversed, setReversed}) {
         <MDBDropdown group>
             <MDBDropdownToggle color="link">Sort</MDBDropdownToggle>
             <MDBDropdownMenu className='p-4 text-muted' style={{ maxWidth: '200px' }}>
-                <MDBRadio name='date' id='date1' label='New to old' checked={!reversed} onChange={() => {setReversed(false); console.log(reversed)}}/>  
-                <MDBRadio name='date' id='date2' label='Old to new' checked={reversed} onChange={() => {setReversed(true); console.log(reversed)}}/>
+                <MDBRadio name='date' id='date1' label='New to old' checked={!reversed} onChange={() => setReversed(false)}/>  
+                <MDBRadio name='date' id='date2' label='Old to new' checked={reversed} onChange={() => setReversed(true)}/>
             </MDBDropdownMenu>
         </MDBDropdown>
     )
