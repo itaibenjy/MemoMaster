f8c196d18c042620816e0822022a4ef09ad4e40b
Itai Benjamin <51812134+itaibenjy@users.noreply.github.com>
2023-06-22 15:06:59 +0300

    deploy

--- Dashboard-client/src/index.js
+++ Dashboard-client/src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 
 const originalFetch = window.fetch;
 window.fetch = function (resource, init) {
-  const serverUrl = "https://memo-master-server-7c0b3fe9dbce.herokuapp.com/";
+  const serverUrl = "https://memo-master-server-7c0b3fe9dbce.herokuapp.com";
   arguments[0] = `${serverUrl}${resource}`;
   return originalFetch.apply(this, arguments);
 };
