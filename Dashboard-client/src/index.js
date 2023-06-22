2328db23386c35529998e7081c441338eae9d6b4
Itai Benjamin <51812134+itaibenjy@users.noreply.github.com>
2023-06-22 14:48:41 +0300

    "deploy vercel"

--- Dashboard-client/src/index.js
+++ Dashboard-client/src/index.js
@@ -6,7 +6,12 @@ import './index.css';
 import { ThemeProvider } from './context/ThemeContext';
 import App from './App';
 
-
+const originalFetch = window.fetch;
+window.fetch = function (resource, init) {
+  const serverUrl = "http://memo-master-server-7c0b3fe9dbce.herokuapp.com";
+  arguments[0] = `${serverUrl}${resource}`;
+  return originalFetch.apply(this, arguments);
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
